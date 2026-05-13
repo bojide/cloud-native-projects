394cb9f00c097ed9fd400fd25b90a9d45e30e205
jideajisafe@gmail.com <jideajisafea@gmail.com>
2026-05-13 13:25:32 -0400

    updated code

--- github-actions-cicd-project/iac/backend.tf
+++ github-actions-cicd-project/iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "cicd-github-actions-terraform-remote-state"
+    key            = "rentzone-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cicd-github-actions-terraform-state-lock"
   }
 }
