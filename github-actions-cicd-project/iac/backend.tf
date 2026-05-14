b9181df4baf8711b47f93663f3529c1123e655c8
Babajide <117398996+bojide@users.noreply.github.com>
2026-05-14 11:35:55 -0400

    Delete github-actions-cicd-project directory

--- github-actions-cicd-project/iac/backend.tf
+++
@@ -1,9 +0,0 @@
-# store the terraform state file in s3 and lock with dynamodb
-terraform {
-  backend "s3" {
-    bucket         = "cicd-github-actions-terraform-remote-state"
-    key            = "rentzone-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "cicd-github-actions-terraform-state-lock"
-  }
-}
